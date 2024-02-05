e7018dbd0e03611274f06db4832e8a0f61425aea
Kuwar shiv pratap singh <102828236+apiguy111@users.noreply.github.com>
2024-02-05 22:07:57 +0530

    Update Widget.jsx

--- client/src/components/widget/Widget.jsx
+++ client/src/components/widget/Widget.jsx
@@ -19,7 +19,7 @@ const Widget = ({ type }) => {
     const fetchData = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:8800/api/dashboard/visualization"
+          "https://dashboard-api-jade.vercel.app/api/dashboard/visualization"
         );
 
         switch (type) {
