6ffae8915a4f5e952cc223c8c94de96c295b045d
Kuwar shiv pratap singh <102828236+apiguy111@users.noreply.github.com>
2024-02-05 21:48:22 +0530

    Update Featured.jsx

--- client/src/components/featured/Featured.jsx
+++ client/src/components/featured/Featured.jsx
@@ -17,7 +17,7 @@ const Featured = () => {
     const fetchData = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:8800/api/dashboard/average"
+          "https://dashboard-api-jade.vercel.app/api/dashboard/average"
         );
 
         const {
