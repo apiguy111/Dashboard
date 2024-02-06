ea435ffe112be1a0293b282fe747a89db4ed037a
Kuwar shiv pratap singh <102828236+apiguy111@users.noreply.github.com>
2024-02-06 16:04:41 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -24,7 +24,9 @@ app.use(
     credentials: true,
   })
 );
-
+app.get('/', (req, res) => {
+  res.json('Hello World!');
+});
 app.use("/api/dashboard", dashboardRoute);
 
 const port = process.env.PORT || 8000;
