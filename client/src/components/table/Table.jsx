a09c37a88e37cb9fba529c5278a009239649b8ac
Kuwar shiv pratap singh <102828236+apiguy111@users.noreply.github.com>
2024-02-08 15:04:08 +0530

    Update Table.jsx

--- client/src/components/table/Table.jsx
+++ client/src/components/table/Table.jsx
@@ -20,7 +20,7 @@ const List = () => {
     const fetchData = async () => {
       try {
         const response = await axios.get(
-          "https://dashboard-3074.onrender.com/api/dashboard/visualization"
+          "/dashboard/visualization"
         );
 
         const { countryDistribution } = response.data;
