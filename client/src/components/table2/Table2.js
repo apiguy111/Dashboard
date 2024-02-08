c8bf9113d8195f0b59fb9c6af95bf4e1c9d665b7
Kuwar shiv pratap singh <102828236+apiguy111@users.noreply.github.com>
2024-02-08 15:04:35 +0530

    Update Table2.js

--- client/src/components/table2/Table2.js
+++ client/src/components/table2/Table2.js
@@ -17,7 +17,7 @@ const List = ({ query, heading }) => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const apiUrl = `https://dashboard-3074.onrender.com/api/dashboard/filter?${heading}=${query}`;
+        const apiUrl = `/dashboard/filter?${heading}=${query}`;
         console.log(apiUrl); // Log the constructed API URL
 
         const response = await axios.get(apiUrl);
